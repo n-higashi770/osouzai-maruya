dcedd23ed0635ea9d6fc0d36435ba2be3f51d10b
n-higashi770 <naoo1500@gmail.com>
2026-05-12 23:16:23 +0900

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,83 @@
+/* =========================
+  スライドショー（そのまま）
+========================= */
+const slides = document.querySelectorAll('.slide-wrap');
+let index = 0;
+
+function showSlide(i) {
+  slides.forEach(s => s.classList.remove('active'));
+  slides[i].classList.add('active');
+}
+
+showSlide(index);
+
+setInterval(() => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+}, 4000);
+
+/* ハンバーガー */
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.global-nav');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  nav.classList.toggle('open');
+});
+
+/* メニュー押したら閉じる */
+document.querySelectorAll('.global-nav a').forEach(link=>{
+  link.addEventListener('click',()=>{
+    hamburger.classList.remove('active');
+    nav.classList.remove('open');
+  });
+});
+
+/* navスクロール */
+window.addEventListener('scroll', () => {
+  const heroHeight = document.getElementById('hero').offsetHeight;
+  const nav = document.querySelector('.pc-horizontal-nav');
+
+  if (window.scrollY > heroHeight) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
+
+/* スワイパー */
+const menuSlider = new Swiper('.menu-slider', {
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  speed: 900,
+
+  autoplay: {
+    delay: 3200,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    0: {
+      spaceBetween: 16
+    },
+    768: {
+      spaceBetween: 20
+    },
+    1200: {
+      spaceBetween: 24
+    }
+  },
+
+  allowTouchMove: true,
+  simulateTouch: true,
+  touchRatio: 1,
+
+  observer: true,
+  observeParents: true
+});
